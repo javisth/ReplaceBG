086950977cc40f7eefdb558f54321f39be26a024
javisth <javisth@gmail.com>
2024-01-24 22:12:10 +0530

    Update app.py

--- removebg/app.py
+++ removebg/app.py
@@ -1,9 +1,12 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify, send_file, url_for
 from flask_cors import CORS
 import background_remover  # Your background removal module
 from PIL import Image
 import qrcode
 import os
+from io import BytesIO
+import base64
+
 
 app = Flask(__name__)
 CORS(app)
@@ -15,6 +18,7 @@ os.makedirs(output_dir, exist_ok=True)
 
 @app.route('/remove-background', methods=['POST'])
 def remove_background():
+    print(request.files)
     if 'image' not in request.files:
         return jsonify({'error': 'No image file'}), 400
 
@@ -34,11 +38,21 @@ def remove_background():
                                                            background_path, logo_path, 
                                                            'Your QR Code Data')
         if final_image_path is not None:
-            return send_file(final_image_path, mimetype='image/png')
+            # Generate QR Code for download
+            qr_code_data = 'Data for QR Code'  # Change this to your desired data
+            qr_code_img = generate_qr_code(qr_code_data)
+            qr_io = BytesIO()
+            qr_code_img.save(qr_io, 'PNG')
+            qr_io.seek(0)
+            qr_code_data_url = 'data:image/png;base64,' + base64.b64encode(qr_io.getvalue()).decode()
+
+            image_url = url_for('static', filename='output/output_image.png', _external=True)
+            return jsonify({'imageUrl': image_url, 'qrCodeDataUrl': qr_code_data_url})
+
         else:
             return jsonify({'error': 'Image processing failed'}), 500
     else:
-            return jsonify({'error': 'Background removal failed'}), 500
+        return jsonify({'error': 'Background removal failed'}), 500
 
 def process_image_with_logo_and_qr(input_path, output_path, background_path, logo_path, qr_data):
     # Load the background image
@@ -82,3 +96,4 @@ def generate_qr_code(data):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
